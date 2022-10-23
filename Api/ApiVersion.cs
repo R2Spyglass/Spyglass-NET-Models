de59a6c14cc3b013d746e1a001e795084b60b16d
Erlite <ys.aameziane@gmail.com>
2022-10-23 15:48:01 +0200

    Add ApiVersion model

---
+++ Api/ApiVersion.cs
@@ -0,0 +1,18 @@
+﻿namespace Spyglass.Models
+{
+    /// <summary>
+    /// Contains data about the API's current version, and the minimum client version required to interact with it.
+    /// </summary>
+    public class ApiVersion : ApiResult
+    {
+        /// <summary>
+        /// The current version of the Spyglass API.
+        /// </summary>
+        public string Version { get; set; } = default!;
+
+        /// <summary>
+        /// The minimum client version required to interact with this API.
+        /// </summary>
+        public string MinimumVersion { get; set; } = default!;
+    }
+}
