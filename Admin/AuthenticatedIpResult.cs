7bb0baa0109a006bcad3e4913db65707449e3e18
Erlite <ys.aameziane@gmail.com>
2023-01-07 18:44:45 +0100

    Add AuthenticatedRequestData model

---
+++ Admin/AuthenticatedIpResult.cs
@@ -0,0 +1,19 @@
+﻿namespace Spyglass.Models.Admin
+{
+    /// <summary>
+    /// The result of an attempt to lookup an ip for an authenticated client.
+    /// </summary>
+    public class AuthenticatedIpResult : ApiResult
+    {
+        /// <summary>
+        /// The ip address that was looked up.
+        /// </summary>
+        public string IpAddress { get; set; } = null!;
+
+        /// <summary>
+        /// List of authenticated requests that match the ip address.
+        /// </summary>
+        public List<AuthenticatedRequestData> Matches { get; set; } = new List<AuthenticatedRequestData>();
+    }
+}
+
---
+++ Admin/AuthenticatedRequestData.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using NpgsqlTypes;
+
+namespace Spyglass.Models.Admin
+{
+    public class AuthenticatedRequestData
+    {
+        /// <summary>
+        /// The id of the client that made the request.
+        /// </summary>
+        public string ClientId { get; set; } = null!;
+        
+        /// <summary>
+        /// If available, the name of the server that made this request.
+        /// </summary>
+        public string? ServerName { get; set; }
+
+        /// <summary>
+        /// IP address of the client that made the authenticated request.
+        /// </summary>
+        public string IpAddress { get; set; } = null!;
+        
+        /// <summary>
+        /// The date and time at which the request from this client and ip was first made at.
+        /// </summary>
+        public DateTimeOffset RequestTime { get; set; }
+    }
+}
+
