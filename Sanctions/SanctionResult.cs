fb684145e3fedd0992c9ea483a67a97850bafc1f
Younes Meziane <ys.aameziane@gmail.com>
2024-07-18 22:13:26 +0200

    Add SanctionResult

---
+++ Sanctions/SanctionResult.cs
@@ -0,0 +1,13 @@
+﻿namespace Spyglass.Models
+{
+    /// <summary>
+    /// Contains the result to a query for all Spyglass sanctions.
+    /// </summary>
+    public class SanctionResult : ApiResult
+    {
+        /// <summary>
+        /// List of all sanctions currently available in the database.
+        /// </summary>
+        public List<PlayerSanction>? Sanctions { get; set; }
+    }
+}
